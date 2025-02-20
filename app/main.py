983ebcced05e1088881fe8e9a7eb184f54340a1e
AlibekovNurtilek <nurtilek007007@gmail.com>
2025-02-21 04:11:04 +0600

    Test CI/CD

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routes import users, items
 
-app = FastAPI(title="Hackathon-2025")
+app = FastAPI(title="Hackathon-2025 API")
 
 # Подключаем маршруты
 app.include_router(users.router, prefix="/users", tags=["Users"])
